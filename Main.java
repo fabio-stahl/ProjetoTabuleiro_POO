91a37f6d5b3281ec982f6000c7a386972c4a9030
GlaucoCiprianoMoreira <glaucodx@gmail.com>
2025-05-15 15:28:44 -0300

    minha branch

--- Main.java
+++ Main.java
@@ -102,7 +102,7 @@ public class Main {
         System.out.println("\nRodada " + rodada + " - Fim de jogo!");
         System.out.println("Jogador " + tabuleiro.getJogadores().get(jogadorVitorioso).getCor() + " vitorioso.");
         System.out.println("Número de jogadas dos jogadores:");
-        for (int j=0; j<tabuleiro.getJogadores.size(); j++) {
+        for (int j=0; j<tabuleiro.getJogadores().size(); j++) {
         	System.out.println("Número de jogadas do jogador " + tabuleiro.getJogadores().get(j).getCor() + " é " + tabuleiro.getJogadores().get(j).getNumJogadas());
         }
     }
